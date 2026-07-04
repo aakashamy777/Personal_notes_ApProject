a4ea81f573697f17a0078ea381dffe4b79ff5ccc
Aakash <aakshmehta2005@gmail.com>
2026-07-04 17:14:56 +0530

    feat: add auth controller and routes

---
+++ src/controllers/authController.js
@@ -0,0 +1,11 @@
+// Auth controller — logic will be added in Stage 2
+
+const register = async (req, res, next) => {
+  // TODO: Implement user registration
+};
+
+const login = async (req, res, next) => {
+  // TODO: Implement user login
+};
+
+module.exports = { register, login };

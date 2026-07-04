1df26d954b42ca18d939ef389a1cdac72689752c
Aakash <aakshmehta2005@gmail.com>
2026-07-04 17:14:56 +0530

    feat: add request validation utilities

---
+++ src/utils/validators.js
@@ -0,0 +1,16 @@
+// Validation rules using express-validator — logic will be added in Stage 2
+const { body } = require('express-validator');
+
+const registerValidation = [
+  // TODO: Add validation rules for registration
+];
+
+const loginValidation = [
+  // TODO: Add validation rules for login
+];
+
+const noteValidation = [
+  // TODO: Add validation rules for note creation
+];
+
+module.exports = { registerValidation, loginValidation, noteValidation };
